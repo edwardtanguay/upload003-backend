a795080ff26c7d0697063565839c1287edf533a5
Edward Tanguay (windows) <edward@tanguay.info>
2023-01-31 01:28:04 +0100

    works

--- src/config.ts
+++
@@ -1,2 +0,0 @@
-export const port = 3610;
-export const logPathAndFileName = './src/logs/log.txt';
--- src/server.ts
+++ src/server.ts
@@ -1,41 +1,67 @@
-import * as model from './model.js';
 import express from 'express';
 import cors from 'cors';
-import logger from './logger.js';
-import * as config from './config.js';
+import multer from 'multer';
+import { join, dirname } from 'path';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { Low, JSONFile } from 'lowdb'; // TODO: check if this is an outdated version of lowdb
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const dbFile = join(__dirname, '../src/data/db.json');
+const adapter = new JSONFile(dbFile);
+const db = new Low(adapter);
 
 const app = express();
-app.use(cors());
-app.use(logger);
+const port = 5891;
 
-app.get('/', (req: express.Request, res: express.Response) => {
-	res.send(model.getApiInstructions());
-});
+// TODO: resolve TypeScript anys in this file
+// TODO: move /public/images/uploadedFiles/general upward out of uploadedFiles
+const staticDirectory = path.join(__dirname, './public');
+app.use(express.static(staticDirectory));
 
-app.get('/books', (req: express.Request, res: express.Response) => {
-	res.json(model.getBooks());
-});
-
-app.get('/books/:id', (req: express.Request, res: express.Response) => {
-	const id = Number(req.params.id);
-	if (isNaN(id)) {
-		res.status(400).send({
-			error: true,
-			message: "sent string, should be number"
-		});
-	} else {
-		const book = model.getBook(id);
-		if (book === undefined) {
-			res.status(404).send({
-				error: true,
-				message: "id did not correspond to an existing item"
-			});
-		} else {
-			res.json(book);
-		}
+const storage = multer.diskStorage({
+	destination: (req: Express.Request, file: any, cb: (arg0: any, arg1: string) => void) => {
+		cb(null, 'public/uploadedFiles/');
+	},
+	filename: (req: Express.Request, file: { originalname: string; }, cb: (arg0: any, arg1: any) => void) => {
+		cb(null, file.originalname);
 	}
 });
 
-app.listen(config.port, () => {
-	console.log(`listening on port http://localhost:${config.port}`);
+const upload = multer({ storage: storage });
+
+app.use(cors());
+
+app.get('/fileitems', async (req, res) => {
+	await db.read();
+	res.send((db.data as any).fileItems);
+});
+
+app.post('/uploadfile', upload.single('file'), async (req: any, res: any) => {
+	await db.read();
+	const fileName = req.body.fileName;
+	let iconPathAndFileName = '';
+	if (fileName.endsWith('.xlsx')) {
+		iconPathAndFileName = 'uploadedFiles/general/iconExcel.png';
+	} else if (fileName.endsWith('.json')) {
+		iconPathAndFileName = 'uploadedFiles/general/iconJson.png';
+	} else if (fileName.endsWith('.txt')) {
+		iconPathAndFileName = 'uploadedFiles/general/iconText.png';
+	} else {
+		iconPathAndFileName = `uploadedFiles/${fileName}`
+	}
+
+	(db.data as any).fileItems.push({
+		title: req.body.title,
+		description: req.body.description,
+		notes: req.body.notes,
+		fileName: req.body.fileName,
+		iconPathAndFileName
+	});
+	await db.write();
+	res.json({});
+});
+
+app.listen(port, () => {
+	console.log(`listening at http://localhost:${port}`);
 });
